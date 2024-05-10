6212156a21834f70aca427791fdc029a9209860f
Yasser Harbi <yasser.harbi@gmail.com>
2024-05-11 01:39:22 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ echo Running Mota API server
 docker exec -d mota-sb /bin/sh -c "mvn spring-boot:run"
 
 echo Running Mota Executor server
-docker exec -d mota-executor /bin/sh -c "mvn spring-boot:run"
+docker exec -d mota-exec /bin/sh -c "mvn spring-boot:run"
 
 echo Running Mota Frontend
 docker exec -it ng-mota /bin/sh -c "npm i & npx nx run ng-mota:serve:development --configuration=development --servePath=/m6/ --host 0.0.0.0"
